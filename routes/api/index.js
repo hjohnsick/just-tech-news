9ca7622f99af52c2f67cd14fb71c8343b54c8ff2
hjohnsick <hjohnsick@yahoo.com>
2022-02-22 21:10:22 -0500

    added ability to comment

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
+// This is what allows you to just use / in the routes.  You are defining here that 
+// the route will be /users and in the index.js under routes you signify it as api
+// so the full path is api/users then the continuation in the other routes file
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
